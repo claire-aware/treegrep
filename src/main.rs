6d87e10434d41989d2854962cebee6f1f86c5f04
Flechette <claire@Flechette.net>
2026-04-25 13:37:26 -0600

    Made arguments more easily modifyable

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,57 @@ use std::fs::File;
 use std::io::{self, BufReader};
 use std::path::Path;
 
-fn main() {
-    let mut tabwidth = 4;
-    let mut patterns: Vec<String> = Vec::new();
-    let mut files: Vec<String> = Vec::new();
-
-    let emptyarguments = env::args().len() <= 1;
-    let mut settingtabwidth = false;
-    let mut addingpattern = false;
-    for arg in env::args() {
-        if emptyarguments || [String::from("-h"), String::from("--help")].contains(&arg) {
-            eprintln!(
-                "Usage: {} [OPTIONS]... PATTERN [FILES]..., {} [OPTIONS INCLUDING -p]... [FILES]...
+fn show_help() {
+    eprintln!(
+        "Usage: {} [OPTIONS]... PATTERN [FILES]..., {} [OPTIONS INCLUDING -p]... [FILES]...
 Options:
     -h, --help: Display this message
     -p [PATTERN], --pattern [PATTERN]: Add pattern to match
-    -t [NUMBER], --tab-width [NUMBER]: Set tab width for tree structure",
-                env::args().next().expect(""),
-                env::args().next().expect("")
-            );
-            std::process::exit(1);
-        } else if [String::from("-t"), String::from("--tab-width")].contains(&arg) {
-            settingtabwidth = true;
-        } else if settingtabwidth {
-            settingtabwidth = false;
-            tabwidth = arg.parse().unwrap();
-        } else if [String::from("-p"), String::from("--pattern")].contains(&arg) {
-            addingpattern = true;
-        } else if patterns.len() <= 0
-            || addingpattern
-            || (files.len() <= 0 && !exists(&arg).unwrap())
-        {
-            addingpattern = false;
-            patterns.push(arg);
+    -t [NUMBER], --tab-width [NUMBER]: Set tab width for tree structure. Defaults to 4",
+        env::args().next().expect(""),
+        env::args().next().expect("")
+    );
+    std::process::exit(1);
+}
+
+fn main() {
+    let mut tab_width = 4;
+    let mut patterns: Vec<String> = Vec::new();
+    let mut files: Vec<String> = Vec::new();
+
+    if env::args().len() <= 2 { //Progam name and file/pattern with no partner
+        show_help();
+    }
+    let mut last_flag: String = String::from("");
+    for arg in env::args().skip(1) { //Skip program name
+        if arg.starts_with("-") {
+            match arg.as_str() {
+                "-h" | "--help" => show_help(),
+                _ => last_flag = arg,
+            }
         } else {
-            files.push(arg);
+            match last_flag.as_str() {
+                "-t" | "--tab_width" => {
+                    tab_width = arg.parse().unwrap();
+                }
+                "-p" | "--pattern" => {
+                    patterns.push(arg);
+                }
+                "" => {
+                    if patterns.len() <= 0 || (files.len() <= 0 && !exists(&arg).unwrap()) {
+                        patterns.push(arg);
+                    } else {
+                        files.push(arg);
+                    }
+                }
+                _ => show_help(),
+            }
         }
     }
 
     if files.len() <= 0 {
         let stdin = io::stdin();
-        treegrep::grep(&patterns, "", stdin.lock(), tabwidth);
+        treegrep::grep(&patterns, "", stdin.lock(), tab_width);
         return;
     }
 
@@ -61,6 +71,6 @@ Options:
         } else {
             ""
         };
-        treegrep::grep(&patterns, filename, BufReader::new(f), tabwidth);
+        treegrep::grep(&patterns, filename, BufReader::new(f), tab_width);
     }
 }
